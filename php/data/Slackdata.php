a7c1fba56510f9ce44a294c44326e417573dc569
xcgpseud <neilyo@hotmail.co.uk>
2016-06-11 00:13:47 +0100

    Moved slackURL to safe location

--- php/data/Slackdata.php
+++ php/data/Slackdata.php
@@ -8,21 +8,21 @@ include 'php/config/slack.php';
  */
 class SlackData{
 
-    private $slackurl = SLACKURL;
+    private $slackUrl = SLACKURL;
     private $username = SLACKUSERNAME;
 
     public function postToSlack($message)
     {
         try{
 
-            $curl = curl_init($this->slackurl);
+            $curl = curl_init($this->slackUrl);
 
             curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false); // TODO: REMOVE BEFORE PUBLISHING
 
-            curl_setopt($curl, CURLOPT_URL, $this->slackurl);
-            curl_setopt($curl,CURLOPT_POST, 1);
-            curl_setopt($curl,CURLOPT_POSTFIELDS, "{\"text\" : \"" . $message . "\", \"username\" : \"Shoutbox-bot\"}");
-            curl_setopt($curl,CURLOPT_HTTPHEADER, array("Content-Type: text/plain"));
+            curl_setopt($curl, CURLOPT_URL, $this->slackUrl);
+            curl_setopt($curl, CURLOPT_POST, 1);
+            curl_setopt($curl, CURLOPT_POSTFIELDS, "{\"text\" : \"" . $message . "\", \"username\" : \"Shoutbox-bot\"}");
+            curl_setopt($curl, CURLOPT_HTTPHEADER, array("Content-Type: text/plain"));
 
             $res = curl_exec($curl);
 
